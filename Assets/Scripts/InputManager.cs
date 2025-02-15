cb2e352782fcd485a07cc8874039353eaa461a75
cartercsm9 <cartercsm9@gmail.com>
2025-02-15 14:24:15 -0800

    pins setup

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+public class InputManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public UnityEvent OnSpacePressed = new UnityEvent();
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke();
+        }
+    }
+}
