dde50837186715f04ffd03c1d237953091be5cf6
cartercsm9 <cartercsm9@gmail.com>
2025-02-15 20:25:26 -0800

    Game Complete

--- Assets/Scripts/LaunchIndicator.cs
+++ Assets/Scripts/LaunchIndicator.cs
@@ -4,11 +4,20 @@ using Unity.Cinemachine;
 public class LaunchIndicator : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera freeLookCamera;
+    
+    // Multiplier to increase (or decrease) the yaw rotation compared to the camera.
+    [SerializeField] private float yawMultiplier = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.forward = freeLookCamera.transform.forward;
-        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y,0);
+        // Get the camera's yaw (horizontal rotation)
+        float cameraYaw = freeLookCamera.transform.eulerAngles.y;
+        
+        // Multiply the yaw by our multiplier
+        float adjustedYaw = cameraYaw * yawMultiplier;
+        
+        // Set the indicator's rotation using the adjusted yaw, while zeroing pitch and roll.
+        transform.rotation = Quaternion.Euler(0, adjustedYaw, 0);
     }
 }
