af64527be165c4c6d7620908e358be62f030114d
cartercsm9 <cartercsm9@gmail.com>
2025-02-15 18:30:49 -0800

    Basic Gameplay Complete

--- Assets/Scripts/LaunchIndicator.cs
+++ Assets/Scripts/LaunchIndicator.cs
@@ -4,17 +4,11 @@ using Unity.Cinemachine;
 public class LaunchIndicator : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera freeLookCamera;
-    
-    // You can expose this to adjust the offset if needed.
-    [SerializeField] private float yOffset = -20f;
 
     // Update is called once per frame
     void Update()
     {
-        // Match the camera's forward vector
         transform.forward = freeLookCamera.transform.forward;
-        // Then reset pitch and roll while applying a -20 degree offset to yaw
-        float adjustedY = transform.rotation.eulerAngles.y + yOffset;
-        transform.rotation = Quaternion.Euler(0, adjustedY, 0);
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y,0);
     }
 }
